247df32788d4d76a781964c76da199888a78facd
Eric McKinney <mckinney99@gmail.com>
2017-12-11 21:55:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,9 @@ class Application
 
     if time.hour <= 12
       resp.write "Good Morning!"
-    else resp.write "Good Afternoon!"
+    else 
+      resp.write "Good Afternoon!"
     end
+    resp.finish
   end
 end
